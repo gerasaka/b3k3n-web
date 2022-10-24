2218ada441eb832e47a82733a58ef073f90b8853
gerasaka <gerryjulioo@gmail.com>
2022-10-24 12:03:11 +0700

    test: add more response data from mock server for category api

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -7,6 +7,9 @@ export const handlers = [
       ctx.json([
         { id: 1, name: 'Happiness \u0026 Mindfulness' },
         { id: 11, name: 'Career \u0026 Business' },
+        { id: 12, name: 'Productivity \u0026 Time Management' },
+        { id: 19, name: 'Society \u0026 Politics' },
+        { id: 21, name: 'Investment \u0026 Finance' },
       ])
     );
   }),
