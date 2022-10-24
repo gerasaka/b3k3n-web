a36cd9f2691309fa6d72572ecb2fc681e2b7bced
gerasaka <gerryjulioo@gmail.com>
2022-10-24 11:07:12 +0700

    test: render App properly

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,8 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders App properly', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const heading = screen.getByText('Hello World');
+  expect(heading).toBeInTheDocument();
 });
