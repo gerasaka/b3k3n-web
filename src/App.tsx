980cc382400504d4bfbc9d9546c2bc8d7ab7e723
gerasaka <gerryjulioo@gmail.com>
2022-10-24 12:22:49 +0700

    feat: render categories from sejutacita api

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+
+type categoryType = {
+  id: string;
+  name: string;
+};
 
 function App() {
-  return <h1>Hello World</h1>;
+  const [categories, setCategories] = useState<categoryType[]>();
+
+  useEffect(() => {
+    fetch('/fee-assessment-categories')
+      .then((res) => res.json())
+      .then((data) => setCategories(data));
+  }, []);
+
+  return categories ? (
+    <ul>
+      {categories.map((category) => {
+        return <li>{category.name}</li>;
+      })}
+    </ul>
+  ) : (
+    <h1>Hello World</h1>
+  );
 }
 
 export default App;
