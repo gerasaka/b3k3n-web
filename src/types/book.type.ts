6694ad6c9010fcc044693090f775afdc7a04fd86
gerasaka <gerryjulioo@gmail.com>
2022-10-26 17:51:30 +0700

    refactor: split type into separate file

---
+++ src/types/book.type.ts
@@ -0,0 +1,13 @@
+export type BookType = {
+  id: number;
+  title: string;
+  category_id: number;
+  authors: string[];
+  cover_url: string;
+  description: string;
+  sections: {
+    title: string;
+    content: string;
+  }[];
+  audio_length: number;
+};
