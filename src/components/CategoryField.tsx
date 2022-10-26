3a8047c475b7a19ba89a9d77f66e0828e59e6162
gerasaka <gerryjulioo@gmail.com>
2022-10-26 17:32:48 +0700

    feat: style category field component

--- src/components/CategoryField.tsx
+++ src/components/CategoryField.tsx
@@ -2,17 +2,47 @@ import React from 'react';
 
 const CategoryField = () => {
   return (
-    <div>
-      <p>Select Category</p>
-      <div>
-        <input type="radio" id="category-1" name="category" />
-        <label htmlFor="category-1">Category 1</label>
+    <div className="text-center">
+      <p className="text-xl font-bold mb-4">Select Category</p>
+      <div className="flex justify-around">
+        <input
+          type="radio"
+          id="category-1"
+          name="category"
+          className="hidden"
+        />
+        <label
+          htmlFor="category-1"
+          className="bg-paradiso text-seashell-peach py-3 px-8 rounded-full"
+        >
+          Category 1
+        </label>
 
-        <input type="radio" id="category-2" name="category" />
-        <label htmlFor="category-2">Category 2</label>
+        <input
+          type="radio"
+          id="category-2"
+          name="category"
+          className="hidden"
+        />
+        <label
+          htmlFor="category-2"
+          className="bg-gainsboro py-3 px-8 rounded-full"
+        >
+          Category 2
+        </label>
 
-        <input type="radio" id="category-3" name="category" />
-        <label htmlFor="category-3">Category 3</label>
+        <input
+          type="radio"
+          id="category-3"
+          name="category"
+          className="hidden"
+        />
+        <label
+          htmlFor="category-3"
+          className="bg-gainsboro py-3 px-8 rounded-full"
+        >
+          Category 3
+        </label>
       </div>
     </div>
   );
