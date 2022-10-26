95189fa7095d8da8fdf09ee8e4c7b8fe8f244d1d
gerasaka <gerryjulioo@gmail.com>
2022-10-26 20:12:27 +0700

    feat: style page control component

--- src/components/PageControl.tsx
+++ src/components/PageControl.tsx
@@ -1,22 +1,26 @@
 import React from 'react';
-import Pagination from './Pagination';
 
-type Props = {
-  children: React.ReactNode;
-};
-
-const PageControl = ({ children }: Props) => {
+const PageControl = () => {
   return (
-    <div>
-      <h2>Happiness & Mindfullness</h2>
-      <hr />
-      <input type="search" name="search" id="search" />
-      <select name="sort" id="sort">
-        <option value="ascending">A - Z</option>
-        <option value="descending">Z - A</option>
-      </select>
-      {children}
-      <Pagination />
+    <div className="max-w-2xl mx-auto mb-12 mt-8">
+      <h2 className="font-serif text-4xl">Happiness & Mindfullness</h2>
+      <hr className="my-4" />
+      <div className="flex gap-6">
+        <input
+          type="search"
+          name="search"
+          id="search"
+          className="h-8 px-3 grow rounded-lg bg-gainsboro"
+        />
+        <select
+          name="sort"
+          id="sort"
+          className="h-8 px-3 rounded-lg bg-gainsboro"
+        >
+          <option value="ascending">A - Z</option>
+          <option value="descending">Z - A</option>
+        </select>
+      </div>
     </div>
   );
 };
