d2fb8fa45c2121b2adf0ed3302c993a0c88c3959
gerasaka <gerryjulioo@gmail.com>
2022-10-28 15:56:01 +0700

    chore: setup `react-router`

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,19 @@
+// TODO: implement semantic tag HTMLs
+
 import React, { useEffect, useState } from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-import CategoryField from './components/CategoryField';
-import Navbar from './components/Navbar';
-import PageControl from './components/PageControl';
-import BookCard from './components/BookCard';
-import Pagination from './components/Pagination';
+import BookDetails from './components/BookDetails';
+import Home from './pages/Home';
 
-import { CategoryType } from './types/category.type';
-import { BookType } from './types/book.type';
-import { ParamType } from './types/param.type';
-
-import { getCategories, getBooks } from './utils/api';
+export const BookContext = React.createContext({});
 
 function App() {
-  const [categories, setCategories] = useState<CategoryType[] | []>([]);
-  const [books, setBooks] = useState<BookType[] | []>([]);
-  const [params, setParams] = useState<ParamType>({
-    categoryId: 1,
-    page: 0,
-  });
-
-  useEffect(() => {
-    getCategories().then((categories) => setCategories(categories));
-  }, []);
-
-  useEffect(() => {
-    getBooks(params).then((books) => setBooks(books));
-  }, [params]);
-
   return (
-    <div className="py-4 max-w-5xl mx-auto">
-      <Navbar />
-      <CategoryField categories={categories} setParams={setParams} />
-      <PageControl />
-      <BookCard books={books} />
-      <Pagination currentPage={params.page} setPage={setParams} />
-    </div>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/books/:id" element={<BookDetails />} />
+    </Routes>
   );
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+
 import App from './App';
 
 import './index.css';
@@ -10,6 +12,8 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
