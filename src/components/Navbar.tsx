da593ee4ee6ee417ebaa225c3deea282f14aadf3
gerasaka <gerryjulioo@gmail.com>
2022-10-28 23:09:20 +0700

    refactor: change `<a>` to `<link>`

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 import logo from '../assets/sejutacita-logo.png';
 
 const Navbar = () => {
   return (
     <nav className="flex justify-between mb-4">
-      <a href="/">
+      <Link to={'/'}>
         <img src={logo} alt="logo" className="h-10" />
-      </a>
-      <a href="/bookmark" className="self-center">
+      </Link>
+      <Link to="/bookmark" className="self-center">
         Bookmark
-      </a>
+      </Link>
     </nav>
   );
 };
