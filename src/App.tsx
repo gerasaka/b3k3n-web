3c695010a20ec2e5e92727e0193d3f88f80887cb
gerasaka <gerryjulioo@gmail.com>
2022-10-30 16:25:02 +0700

    perf: move side effects to App component

--- src/App.tsx
+++ src/App.tsx
@@ -4,24 +4,56 @@ import React, { useEffect, useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 
 import BookDetails from './components/BookDetails';
+import Navbar from './components/Navbar';
 import Bookmark from './pages/Bookmark';
 import Home from './pages/Home';
 
-import { BookType } from './types/book.type';
+import { CategoryType, ParamType, BookType } from './types';
+import { getBooks, getCategories } from './utils/api';
 
 function App() {
+  const [categories, setCategories] = useState<CategoryType[] | []>([]);
+  const [params, setParams] = useState<ParamType>({
+    categoryId: 1,
+    page: 0,
+  });
+
+  const [books, setBooks] = useState<BookType[] | []>([]);
+  const [filteredBooks, setFilteredBooks] = useState<BookType[] | []>([]);
+
   const [bookmarkedBooks, setBookmarkedBooks] = useState<BookType[] | []>([]);
 
   useEffect(() => {
-    const bookmark: string = localStorage.getItem('b3k3n-books') || '[]';
+    getCategories().then((categories) => setCategories(categories));
 
+    const bookmark: string = localStorage.getItem('b3k3n-books') || '[]';
     if (bookmark.length > 2) setBookmarkedBooks(JSON.parse(bookmark));
   }, []);
 
+  useEffect(() => {
+    getBooks(params).then((books) => {
+      setBooks(books);
+      setFilteredBooks(books);
+    });
+  }, [params]);
+
   return (
     <div className="py-4 max-w-5xl mx-auto">
+      <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
+        <Route
+          path="/"
+          element={
+            <Home
+              categories={categories}
+              books={books}
+              params={params}
+              setParams={setParams}
+              filteredBooks={filteredBooks}
+              setFilteredBooks={setFilteredBooks}
+            />
+          }
+        />
         <Route
           path="/books/:id"
           element={
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,38 +1,29 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 
 import BookCard from '../components/BookCard';
 import CategoryField from '../components/CategoryField';
-import Navbar from '../components/Navbar';
 import PageControl from '../components/PageControl';
 import Pagination from '../components/Pagination';
 
-import { BookType } from '../types/book.type';
-import { CategoryType } from '../types/category.type';
-import { ParamType } from '../types/param.type';
+import { BookType, CategoryType, ParamType } from '../types';
 
-import { getBooks, getCategories } from '../utils/api';
+type Props = {
+  categories: [] | CategoryType[];
+  books: [] | BookType[];
+  params: ParamType;
+  setParams: React.Dispatch<React.SetStateAction<ParamType>>;
+  filteredBooks: [] | BookType[];
+  setFilteredBooks: React.Dispatch<React.SetStateAction<[] | BookType[]>>;
+};
 
-const Home = () => {
-  const [categories, setCategories] = useState<CategoryType[] | []>([]);
-  const [books, setBooks] = useState<BookType[] | []>([]);
-  const [params, setParams] = useState<ParamType>({
-    categoryId: 1,
-    page: 0,
-  });
-
-  const [filteredBooks, setFilteredBooks] = useState<BookType[] | []>([]);
-
-  useEffect(() => {
-    getCategories().then((categories) => setCategories(categories));
-  }, []);
-
-  useEffect(() => {
-    getBooks(params).then((books) => {
-      setBooks(books);
-      setFilteredBooks(books);
-    });
-  }, [params]);
-
+const Home = ({
+  categories,
+  books,
+  params,
+  setParams,
+  filteredBooks,
+  setFilteredBooks,
+}: Props) => {
   const handleSearch = (query: string) => {
     const isTitleMatch = (title: string) => title.toLowerCase().includes(query);
     const isAuthorMatch = (authors: string[]) => {
@@ -51,7 +42,6 @@ const Home = () => {
 
   return (
     <div>
-      <Navbar />
       <CategoryField categories={categories} setParams={setParams} />
       <PageControl search={handleSearch} />
       <BookCard books={filteredBooks} />
