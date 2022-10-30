6968e7ed774838f8347c550d89452d98299eb761
gerasaka <gerryjulioo@gmail.com>
2022-10-30 20:08:03 +0700

    feat: make book details page responsive to mobile

--- src/pages/BookDetails.tsx
+++ src/pages/BookDetails.tsx
@@ -43,50 +43,73 @@ const BookDetails = ({ bookmarkList, setBookmarkList }: Props) => {
   };
 
   return (
-    <div className="mt-8 w-10/12 mx-auto">
-      <div className="flex gap-10">
-        <img src={book.cover_url} alt="book cover" className="w-72" />
-        <div>
-          <h1 className="font-serif text-4xl">{book.title}</h1>
-          <h3 className="text-xl mb-4 mt-2">{book.authors.join(', ')}</h3>
+    <div className="mt-6 lg:mt-8 lg:w-10/12 mx-auto">
+      <div className="flex flex-col lg:flex-row gap-4 lg:gap-10">
+        <div className="mx-auto">
+          <img
+            src={book.cover_url}
+            alt="book cover"
+            className="h-60 lg:h-auto lg:w-72"
+          />
+        </div>
+        <div className="text-center lg:text-left">
+          <h1 className="font-serif text-2xl lg:text-4xl">{book.title}</h1>
+          <h3 className="text-sm lg:text-xl mb-4 lg:mt-2">
+            {book.authors.join(', ')}
+          </h3>
 
-          <div className="w-2/3 flex gap-6">
+          <div className="w-full lg:w-2/3 flex gap-4 lg:gap-6 text-sm lg:text-base">
             <button className="h-9 w-full rounded-lg bg-sinbad">Listen</button>
             <button className="h-9 w-full rounded-lg bg-paradiso text-seashell-peach">
               Read
             </button>
           </div>
 
-          <div className="flex gap-6 items-center border-t-2 border-b-2 border-gainsboro p-1 my-4">
-            <p>{book.sections.length} chapters</p>
-            <p>{Math.ceil(book.audio_length / 60)} minutes</p>
+          <div className="flex gap-6 items-center border-t-2 border-b-2 border-gainsboro px-4 lg:p-1 mt-4 mb-2 lg:mb-4">
+            <p className="text-xs lg:text-base">
+              {book.sections.length} chapters
+            </p>
+            <p className="text-xs lg:text-base">
+              {Math.ceil(book.audio_length / 60)} minutes
+            </p>
             <button
               className="h-8 px-3 rounded-lg ml-auto"
               onClick={handleToogleBookmark}
             >
-              Bookmark
-              <span className="p-2 m-2">O</span>
+              <span className="hidden lg:inline mr-2">Bookmark</span>
+              <span className="bg-paradiso inline-block h-5 w-5 lg:h-6 lg:w-6 rounded-full">
+                O
+              </span>
             </button>
           </div>
 
-          <h2 className="font-serif text-2xl mb-2">What's it about</h2>
-          <p>{book.description}</p>
+          <h2 className="font-serif text-xl lg:text-2xl mb-2">
+            What's it about
+          </h2>
+          <p className="text-left px-2 lg:px-0 text-sm lg:text-base">
+            {book.description}
+          </p>
         </div>
       </div>
 
       <div className="max-w-3xl mx-auto">
-        <h2 className="font-serif text-2xl my-4">What's inside</h2>
+        <h2 className="font-serif text-center lg:text-left text-xl lg:text-2xl mt-4 mb-2 lg:mb-4">
+          What's inside
+        </h2>
         {book.sections.map((section: Section, idx: string) => (
-          <div key={idx}>
+          <div key={idx} className="p-2 lg:p-0">
             <input
               type="checkbox"
               id={idx}
-              className="w-full max-w-4xl mx-auto h-5 inset-x-0 peer checked:bg-gainsboro absolute hidden peer"
+              className="w-full max-w-4xl mx-auto h-3 lg:h-5 inset-x-0 peer checked:bg-gainsboro absolute hidden peer"
             />
-            <label htmlFor={idx} className="block h-5 pb-10 pt-4 font-bold">
+            <label
+              htmlFor={idx}
+              className="block h-3 lg:h-5 pb-10 lg:pt-4 font-bold text-sm lg:text-base"
+            >
               {`${idx + 1} ${section.title}`}
             </label>
-            <span className="hidden peer-checked:block p-2 bg-sinbad">
+            <span className="hidden peer-checked:block p-2 bg-sinbad text-sm lg:text-base">
               {section.content}
             </span>
             <hr className="text-gainsboro" />
