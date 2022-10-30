a6814c8fb702b80262f03ec8fa1ccdf1bb02f56b
gerasaka <gerryjulioo@gmail.com>
2022-10-30 15:12:35 +0700

    feat: create function to search book

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -12,11 +12,7 @@ import { ParamType } from '../types/param.type';
 
 import { getBooks, getCategories } from '../utils/api';
 
-type Props = {
-  setBookmark: React.Dispatch<React.SetStateAction<[] | BookType[]>>;
-};
-
-const Home = ({ setBookmark }: Props) => {
+const Home = () => {
   const [categories, setCategories] = useState<CategoryType[] | []>([]);
   const [books, setBooks] = useState<BookType[] | []>([]);
   const [params, setParams] = useState<ParamType>({
@@ -24,20 +20,41 @@ const Home = ({ setBookmark }: Props) => {
     page: 0,
   });
 
+  const [filteredBooks, setFilteredBooks] = useState<BookType[] | []>([]);
+
   useEffect(() => {
     getCategories().then((categories) => setCategories(categories));
   }, []);
 
   useEffect(() => {
-    getBooks(params).then((books) => setBooks(books));
+    getBooks(params).then((books) => {
+      setBooks(books);
+      setFilteredBooks(books);
+    });
   }, [params]);
+
+  const handleSearch = (query: string) => {
+    const isTitleMatch = (title: string) => title.toLowerCase().includes(query);
+    const isAuthorMatch = (authors: string[]) => {
+      return (
+        authors.filter((author) => author.toLowerCase().includes(query))
+          .length > 0
+      );
+    };
+
+    const searchBooks = books.filter(
+      (book) => isTitleMatch(book.title) || isAuthorMatch(book.authors)
+    );
+
+    setFilteredBooks(searchBooks);
+  };
 
   return (
     <div>
       <Navbar />
       <CategoryField categories={categories} setParams={setParams} />
-      <PageControl />
-      <BookCard books={books} />
+      <PageControl search={handleSearch} />
+      <BookCard books={filteredBooks} />
       <Pagination currentPage={params.page} setPage={setParams} />
     </div>
   );
